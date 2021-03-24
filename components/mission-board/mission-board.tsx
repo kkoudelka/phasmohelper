46a38e551d875f44d058294727a1ef9be559239e
Karel Koudelka <k.koudelka01@gmail.com>
2021-03-24 19:38:15 +0100

    feat: show game-time

--- components/mission-board/mission-board.tsx
+++ components/mission-board/mission-board.tsx
@@ -1,13 +1,26 @@
+import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import React from 'react';
 import { DifficultyDisplay, GhostName, ObjectivesContainer } from '.';
-import { HuntTimer } from '../timer';
+import { useAppContext } from '../../src/hooks';
+import { GameTime, HuntTimer } from '../timer';
 
 const MissionBoard: React.FC = () => {
+  const { reset } = useAppContext();
+
   return (
     <div>
-      <Typography variant="h6">Mission board</Typography>
+      <Grid container justify="space-between">
+        <Grid item>
+          <Typography variant="h6">Mission board</Typography>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" color="primary" onClick={reset}>
+            Start new mission
+          </Button>
+        </Grid>
+      </Grid>
       <Grid container direction="row" spacing={1}>
         <Grid item xs={12} md={4} container direction="column" spacing={1}>
           <Grid item>
@@ -15,6 +28,9 @@ const MissionBoard: React.FC = () => {
           </Grid>
           <Grid item>
             <DifficultyDisplay />
+          </Grid>
+          <Grid item>
+            <GameTime />
           </Grid>
           <Grid item>
             <HuntTimer />
--- components/timer/index.ts
+++ components/timer/index.ts
@@ -1,3 +1,5 @@
 export { default as TimerDisplay } from './timer-display';
 
 export { default as HuntTimer } from './hunt-timer';
+
+export { default as GameTime } from './game-time';
